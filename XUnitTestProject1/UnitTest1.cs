aaadd7c5276bc156afc8bc4318e96439b4b5f690
GGKTECH\bhanuprakash.alla <bhanuprakash.alla@ggktech.com>
2018-03-20 14:34:27 +0530

    Added test case.

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -5,10 +5,13 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// added test case.
+        /// </summary>
         [Fact]
         public void Test1()
         {
-
-        }
+            Assert.True(true);
+        } 
     }
 }
